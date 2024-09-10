9eacf8e0e1c27ffa70541d27f218d47067e1205f
Kaiwen Zhu <zhukaiwen113000@gmail.com>
2024-09-10 21:47:32 +1000

    Testing Push

--- src/KevjaTestQuackle.jl
+++ src/KevjaTestQuackle.jl
@@ -1,5 +1,17 @@
 module KevjaTestQuackle
-
+    """ Test module.  Eventually would like to include all common files I use."""
 # Write your package code here.
+
+
+## CHECKING PUSHING UPDATE ##.
+
+## Import dependencies?
+
+## Export functions from other files in ~/src
+
+## Include other files.
+# Catetogirse functions into files:  Geometries and grid init.
+# Solvers
+# Plotting tools, defaults etc.  
 
 end
